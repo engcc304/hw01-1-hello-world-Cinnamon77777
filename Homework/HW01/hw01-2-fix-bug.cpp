a96e4b918f4b56c01cc0773b899d62f7a40b3d63
your name <sriwljitthitima@gmail.com>
2023-07-27 15:17:19 +0700

    up

--- Homework/HW01/hw01-2-fix-bug.cpp
+++ Homework/HW01/hw01-2-fix-bug.cpp
@@ -1,17 +1,9 @@
 //TODO: #6 Fix all bugs.
 #include <stdio.h>
-<<<<<<< HEAD
-
-int main() {
-    printf("Hello, I'm Javis (RMUTL-Version).\n");
-    printf("This is my first time coding with the C language.\n");
+int main()
+{
+    printf("Hello,I'm Javis (RMUTL-Version).\n");
+    printf("This is my first time to coding with c language.\n");
     printf("Today is my birthday.\n");
-    return 0;
-=======
-int main()  {
-    printf( "Hello, I'm Javis (RMUTL-Version).\n" ) ;
-    printf( "This is my first time to coding with C language.\n" ) ;
-    printf( "Today is my birthday.\n" ) ;
-        return '0' ;
->>>>>>> 195763bb7bbcda01bdd5cbe94205849c5ba76b9b
+    return 0 ;
 }
